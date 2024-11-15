530f322880b51852f43e44cd1840f53b6e7335a4
Li Songtao <songtao.li.0x00@gmail.com>
2024-11-15 15:14:41 +0800

    feat: initialize Fincrew project with basic structure and configurations
    - Add project structure and necessary files for a multi-agent AI system
    - Define researchers and reporting analysts as agents with specific roles and goals
    - Create tasks for research and reporting, assigning them to respective agents
    - Implement Fincrew class to manage agents and tasks
    - Add main.py for running, training, and testing the crew
    - Include custom tool example for future enhancements
    - Set up .gitignore and README.md for project maintenance and documentation

---
+++ src/fincrew/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from fincrew.crew import Fincrew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    Fincrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Fincrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        Fincrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Fincrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
